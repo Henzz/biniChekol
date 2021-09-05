80dbcc5f54ba74e23abce1df6159efaaaf8338be
Henzz <khenok53@gmail.com>
2021-09-05 12:18:16 +0300

    setup for all

---
+++ src/components/include/navLanding.vue
@@ -0,0 +1,5 @@
+<template>
+   <div>
+      <h2>this is</h2>
+   </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "admin-lte/dist/css/adminlte.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+require("bootstrap");
+require("admin-lte");
+require("jquery");
+
+window.Popper = require('popper.js').default;
+window.$ = window.jQuery = require('jquery');
+
 Vue.config.productionTip = false;
 
 new Vue({
